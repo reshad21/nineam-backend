84ff88434b1e46941f1bde874db6cd975e660ef0
reshad21 <miakala7@gmail.com>
2024-09-22 16:24:42 +0600

    view page transfer to public folder

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import path from 'path';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
 
 const app: Application = express();
-
+app.use(express.static(path.join(__dirname, "../public/")))
 //parsers
 app.use(express.json());
-app.use(cors({origin:'https://bike-rental-website.vercel.app',credentials:true}));
+app.use(cors({ origin: 'https://bike-rental-website.vercel.app', credentials: true }));
 
 // application routes
 app.use('/api', router);
