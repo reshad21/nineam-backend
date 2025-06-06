a514258a9ad6effd5e1fecff4eaf7c4a10643830
reshad21 <miakala7@gmail.com>
2025-06-06 11:30:43 +0600

    server

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use(express.json())
 
 const corsOptions = {
   origin: "https://nine-am-frontend.vercel.app",
-  methods: ["GET", "POST", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
